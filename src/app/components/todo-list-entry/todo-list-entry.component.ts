d9a8cd5a53d1fcaef2b2adc7f2cb3a05f4013f30
Ikromer-SE <ikromermobile@gmail.com>
2021-08-02 15:25:31 -0400

    done with prototype

--- src/app/components/todo-list-entry/todo-list-entry.component.ts
+++ src/app/components/todo-list-entry/todo-list-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-todo-list-entry',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListEntryComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      item: ['Clean Garage']
+    });
+  }
+
+  addItem() {
+    console.log(this.form.value);
   }
 
 }
--- src/app/components/todo-list-items/todo-list-items.component.ts
+++ src/app/components/todo-list-items/todo-list-items.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list-items.component.css']
 })
 export class TodoListItemsComponent implements OnInit {
+  items: ToDoItemModel[] = [
+    { description: 'Mow Lawn' },
+    { description: 'Empty Boxes from Garage' }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+}
+
+interface ToDoItemModel {
+  description: string;
+
 
 }
