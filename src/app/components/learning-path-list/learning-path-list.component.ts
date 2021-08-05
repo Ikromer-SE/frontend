945305fda92ed3955ec27576f44edb2ebba8a544
Ikromer-SE <ikromermobile@gmail.com>
2021-08-05 15:48:40 -0400

    create learning state

--- src/app/components/learning-path-list/learning-path-list.component.ts
+++ src/app/components/learning-path-list/learning-path-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { LearningListItem } from 'src/app/models/learning.models';
 
 @Component({
   selector: 'app-learning-path-list',
   templateUrl: './learning-path-list.component.html',
   styleUrls: ['./learning-path-list.component.css']
 })
+
 export class LearningPathListComponent implements OnInit {
+
+  items: LearningListItem[] = [
+    { id: '1', topic: 'Docker', competency: 'Competency' },
+    { id: '2', topic: 'Jazz Piano', competency: 'None', notes: 'Give up' }
+  ];
 
   constructor() { }
 
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,13 +1,24 @@
-import * as fromCounter from './counter.reducer'
+import * as fromCounter from './counter.reducer';
+import * as fromLearning from './learning.reducer';
+
+
 
 export interface AppState {
-  counter: fromCounter.CounterState
+  counter: fromCounter.CounterState,
+  learning: fromLearning.LearningState
 }
+
+
 
 export const reducers = {
-  counter: fromCounter.reducer
+  counter: fromCounter.reducer,
+  learning: fromLearning.reducer
 }
 
+
+
 // selector functions
+
+
 
 export const selectCounterCurrent = (state: AppState) => state.counter.current;
