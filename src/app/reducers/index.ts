0298d13d5c813258c29a46fd936b2c551c6e43f6
Ikromer-SE <ikromermobile@gmail.com>
2021-08-06 11:15:42 -0400

    add learning selector

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,24 +1,41 @@
+import { createSelector } from '@ngrx/store';
+import { LearningListItem } from '../models/learning.models';
 import * as fromCounter from './counter.reducer';
 import * as fromLearning from './learning.reducer';
-
-
 
 export interface AppState {
   counter: fromCounter.CounterState,
   learning: fromLearning.LearningState
 }
 
-
-
 export const reducers = {
   counter: fromCounter.reducer,
   learning: fromLearning.reducer
 }
 
-
-
 // selector functions
 
+// 1. Feature Selector (optional - only if you are using feature modules)
 
+// 2. Create a function (selector) for each main branch of your state.
+const selectCounterBranch = (state: AppState) => state.counter;
+const selectLearningBranch = (state: AppState) => state.learning;
 
-export const selectCounterCurrent = (state: AppState) => state.counter.current;
+// 3. Any helpers (non-exported selectors)
+const { selectAll: selectLearningEntityArray } = fromLearning.adapter.getSelectors(selectLearningBranch);
+
+// 4. What ever you component needs. // createSelector does "memoizaiton" - which is like caching (like 'take a memo')
+export const selectCounterCurrent = createSelector(
+  selectCounterBranch,
+  c => c.current
+);
+
+// TODO - select some data that returns a LearningListItem[]
+
+//Tedious way to do it
+// export const selectLearningListItems = (state: AppState) => state.learning.ids.map(id => state.learning.entities[id] as LearningListItem);
+
+export const selectLearningListItems = createSelector(
+  selectLearningEntityArray,
+  items => items as LearningListItem[]
+)
